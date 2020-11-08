2705d3002fdb9744166f2c595a94a06d04fdc6ac
bmuhamood <bbosa2009@gmail.com>
2020-11-08 14:44:57 -0800

    addaed foreign keys to the model

--- accounts/models.py
+++ accounts/models.py
@@ -32,7 +32,9 @@ class Order(models.Model):
 	customer = models.ForeignKey(Customer, null=True, on_delete = models.SET_NULL)
 	product = models.ForeignKey(Product, null=True, on_delete = models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	tags = models.ManyToManyField(Tag)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
+
 
 		
 		
